9de03585df7ef091ccab5edd1e3fc24c2c64d228
zernpany-fs <zern.pany@gmail.com>
2026-05-29 15:14:25 +0700

    Fetch data, Add isError, isLoading, then map data for TodoList

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,38 @@
+import { useQuery } from "@tanstack/react-query";
 import { getTodos } from "../api/todo";
 import TodoItem from "./TodoItem";
 
 getTodos().then((data) => console.log(data));
 
 export default function TodoList() {
+  const {
+    data: todos,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["todos"], //! catching data
+    queryFn: getTodos, //! which function fetch array, [todo object]
+    //! cache ==> { "todos" name key : "fetch" data from server} and only work once if there is identical name, no more fetching
+  });
+
+  if (isLoading)
+    return <p className="text-center text-blue-400">Loading...🤑</p>;
+  if (isError)
+    return (
+      <p className="text-center text-red-400">Somethign went wrong...😑</p>
+    );
+
   return (
     <ul className="space-y-1.5">
-      <TodoItem id={1} title="test1" completed={true} />
-      <TodoItem id={2} title="test2" completed={false} />
-      <TodoItem id={3} title="test3" completed={false} />
+      {" "}
+      {todos.map((t) => (
+        <TodoItem key={t.id} {...t} />
+      ))}{" "}
     </ul>
+    // <ul className="space-y-1.5">
+    //   <TodoItem id={1} title="test1" completed={true} />
+    //   <TodoItem id={2} title="test2" completed={false} />
+    //   <TodoItem id={3} title="test3" completed={false} />
+    // </ul>
   );
 }
